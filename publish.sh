9b28d797b1fe5abe4ce48539fac9e767d34b5cd7
JPBM135 <jpedrobm0@gmail.com>
2024-01-16 12:49:39 -0800

    Release 1.0.16

---
+++ publish.sh
@@ -0,0 +1,7 @@
+VERSION=$(node -p -e "require('./package.json').version")
+
+yarn build
+git add --all
+git commit -m "Release $VERSION" --allow-empty
+git tag $VERSION -m "Release $VERSION"
+git push --follow-tags
