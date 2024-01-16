92184892cc2d4c1538022b7d8ccc2bc428be3e52
JPBM135 <jpedrobm0@gmail.com>
2024-01-16 14:27:51 -0800

    refactor(exec): use sudo

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
+import type { Buffer } from 'node:buffer';
 import process from 'node:process';
 import * as core from '@actions/core';
-import { polyfillYarn } from './polyfills/yarn.js';
+import * as exec from '@actions/exec';
 
 const POLYFILLS = {
-  yarn: polyfillYarn,
+  yarn: 'sudo apt-get install -y --no-install-recommends yarn',
+  curl: 'sudo apt-get install -y --no-install-recommends curl',
+  git: 'sudo apt-get install -y --no-install-recommends git',
+  jq: 'sudo apt-get install -y --no-install-recommends jq',
 };
 
 try {
@@ -18,14 +22,43 @@ try {
     .split(',')
     .map((module) => module.trim());
 
-  for (const [polyfill, polyfillFunction] of Object.entries(POLYFILLS)) {
+  if (ignoredModules.length === Object.keys(POLYFILLS).length) {
+    throw new Error('All polyfills are ignored. Please check your configuration.');
+  }
+
+  core.debug('Installing dependencies...');
+  await exec.exec('sudo apt-get update', undefined, {
+    listeners: {
+      stdout: (data: Buffer) => {
+        core.info(data.toString());
+      },
+      stderr: (data: Buffer) => {
+        core.info(data.toString());
+      },
+    },
+  });
+
+  for (const [polyfill, polyfillCommand] of Object.entries(POLYFILLS)) {
     if (ignoredModules.includes(polyfill)) {
       core.info(`Skipping ${polyfill} polyfill...`);
       continue;
     }
 
     core.info(`Running ${polyfill} polyfill...`);
-    await polyfillFunction();
+    const code = await exec.exec(polyfillCommand, undefined, {
+      listeners: {
+        stdout: (data: Buffer) => {
+          core.info(data.toString());
+        },
+        stderr: (data: Buffer) => {
+          core.info(data.toString());
+        },
+      },
+    });
+
+    if (code !== 0) {
+      throw new Error(`Polyfill ${polyfill} failed with exit code ${code}.`);
+    }
   }
 } catch (error) {
   core.debug(String(error));
