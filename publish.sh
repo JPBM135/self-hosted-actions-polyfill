c088efa69c559995a67811f634f2204d87adb73c
JPBM135 <jpedrobm0@gmail.com>
2024-01-16 23:04:05 -0800

    fix(publish): check if version exists

--- publish.sh
+++ publish.sh
@@ -1,5 +1,11 @@
 VERSION=v$(node -p -e "require('./package.json').version")
 
+# Check if the package version is already published
+if git rev-parse $VERSION >/dev/null 2>&1; then
+  echo "Version $VERSION already published"
+  exit 1
+fi
+
 yarn build
 git add --all
 git commit -m "Release $VERSION" --allow-empty
